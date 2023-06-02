13dbc28a8974c72d62796b87550f437347d95ff6
damien <contact.damiendiaz@gmail.com>
2023-06-02 17:24:07 +0200

    hachage password

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
@@ -19,10 +20,8 @@ class RegisterController extends AbstractController
         $this->entityManager = $entityManager;
     }
 
-
-
     #[Route('/inscription', name: 'app_register')]
-    public function index(Request $request): Response
+    public function index(Request $request, UserPasswordHasherInterface $hasher): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
@@ -30,7 +29,11 @@ class RegisterController extends AbstractController
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()) {
+
             $user = $form->getData();
+
+            $password = $hasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($password);
 
 
             $this->entityManager->persist($user);
