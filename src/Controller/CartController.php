251dde6bbaafa60d0f8c685edd98d27a8516b1bd
damien <contact.damiendiaz@gmail.com>
2023-06-08 16:40:10 +0200

    creation view panier

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,17 +3,36 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Entity\Item;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/mon-panier', name: 'cart')]
+
     public function index(Cart $cart): Response
     {
-        dd($cart->get());
-        return $this->render('cart/index.html.twig');
+        $cartComplete = [];
+
+        foreach ($cart->get() as $id => $quantity) {
+            $cartComplete[] = [
+                'item' => $this->entityManager->getRepository(Item::class)->findOneById($id),
+                'quantity' => $quantity
+            ];
+        }
+
+        return $this->render('cart/index.html.twig', [
+           'cart'=> $cartComplete
+            ]);
     }
 
     #[Route('/cart/add/{id}', name: 'add_to_cart')]
